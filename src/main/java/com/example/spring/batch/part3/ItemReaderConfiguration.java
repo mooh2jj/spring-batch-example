67bc2757bba08ee8f2b75b02a6b7a8340703fbef
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-10-21 15:57:56 +0900

    [add]csv 파일, jdbc 데이터 읽기 구현

--- src/main/java/com/example/spring/batch/part3/ItemReaderConfiguration.java
+++ src/main/java/com/example/spring/batch/part3/ItemReaderConfiguration.java
@@ -7,10 +7,19 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,13 +31,16 @@ public class ItemReaderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
-    public Job itemReaderJob() {
+    public Job itemReaderJob() throws Exception {
 
         return this.jobBuilderFactory.get("itemReaderJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.cutomItemReaderStep())
+                .next(this.csvFileStep())
+                .next(this.jdbcStep())
                 .build();
     }
 
@@ -39,6 +51,67 @@ public class ItemReaderConfiguration {
                 .reader(new CustomItemReader<>(getItems()))
                 .writer(itemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step csvFileStep() throws Exception {
+        return stepBuilderFactory.get("csvFileStep")
+                .<Person, Person>chunk(10)
+                .reader(this.csvFileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcStep() throws Exception {
+        return stepBuilderFactory.get("jdbcStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        var itemReader = new JdbcCursorItemReaderBuilder<Person>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("select id, name, age, address from person")
+                .rowMapper((rs, rowNum) -> new Person(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(2)))
+                .build();
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+
+    private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+
+        var tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id", "name", "age", "address");
+
+        lineMapper.setLineTokenizer(tokenizer);
+
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            int id = fieldSet.readInt("id");
+            String name = fieldSet.readString("name");
+            String age = fieldSet.readString("age");
+            String address = fieldSet.readString("address");
+
+            return new Person(id, name, age, address);
+        });
+
+        var itemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("csvFileItemReader")
+                .encoding("UTF-8")
+                .resource(new ClassPathResource("test.csv"))
+                .linesToSkip(1) // 1line skip 두번째부터 읽는다.
+                .lineMapper(lineMapper)
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
     }
 
     private ItemWriter<Person> itemWriter() {
---
+++ src/main/resources/person.sql
@@ -0,0 +1,13 @@
+create table person (
+    id bigint primary key auto_increment,
+    name varchar(255),
+    age varchar(255),
+    address varchar(255)
+);
+
+insert into person(name, age, address)
+values ('도성곤', '31', '기흥');
+insert into person(name, age, address)
+values ('홍길동', '32', '서울');
+insert into person(name, age, address)
+values ('이상실', '30', '강원');
